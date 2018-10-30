31dfebe8332fa4729b3ada97776bbbf516428478
Marvin Jayson Baga <mjbaga@yahoo.com>
2018-10-31 00:38:39 +0800

    Set base plugin name and author credentials

--- wp-content/plugins/base-plugin/base-plugin.php
+++ wp-content/plugins/base-plugin/base-plugin.php
@@ -1,3 +1,11 @@
 <?php
 
-// Create base plugin for Worpress essentials
+/*
+    Plugin Name: Base Plugin
+    Description: Bespoke Plugin for Wordpress essentials
+    Version: 1.0
+    Author: mjbaga@yahoo.com
+*/
+
+if( !defined('ABSPATH') )
+    die('Access denied.');
